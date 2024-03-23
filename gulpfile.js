9655a3c17d2c05668775e09c797c96010031c954
Alucard-P <paulmct93@gmail.com>
2024-03-22 19:58:48 -0500

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,46 @@ function buildStyles() {
     .pipe(server.stream());
 }
 
+function compilePug() {
+  return gulp
+    .src("dev/pug/*.pug")
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest("./public/"));
+}
+// gulp.task("pug", () => {
+//   return gulp
+//     .src("dev/pug/*.pug")
+//     .pipe(pug({ pretty: true }))
+//     .pipe(gulp.dest("./public/"));
+// });
+
+gulp.task("reload", () => {
+  server.reload;
+});
+
+exports.compilePug = compilePug;
 exports.buildStyles = buildStyles;
-exports.watch = function () {
-  gulp.watch("dev/scss/**/*.scss", ["sass"]);
-};
+
+// exports.watch = function () {
+//   gulp.watch("dev/scss/**/*.scss", ["sass"]);
+//   gulp.watch("dev/pug/**/*.pug", ["pug", "reload"]);
+// };
+
+// gulp.task("default", () => {
+//   server.init({
+//     server: "./public/",
+//   });
+//   gulp.watch("dev/pug/**/*.pug", ["pug", "reload"]);
+//   gulp.watch("dev/scss/**/*.scss", ["sass"]);
+//   gulp.watch("public/index.html", server.reload);
+// });
+
+gulp.task("default", () => {
+  server.init({
+    server: "./public/",
+  });
+
+  gulp.watch("dev/pug/**/*.pug", compilePug);
+  gulp.watch("dev/scss/**/*.scss", buildStyles);
+  gulp.watch("public/index.html").on("change", server.reload);
+});
